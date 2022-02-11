deeec2ac6d8b6a14a004e513c7af56c496465605
Sanskar00 <saxenasanskar090901@gmail.com>
2022-02-11 18:02:19 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,8 +9,6 @@ connectDB();
 //init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API RUNNING"));
-
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
@@ -17,6 +16,16 @@ app.use("/api/product", require("./routes/api/product"));
 app.use("/api/personalInfo", require("./routes/api/personalInfo"));
 app.use("/api/cart", require("./routes/api/cart"));
 app.use("/api/order", require("./routes/api/order"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
