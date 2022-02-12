2879276e248d8b8589f0ab5dcfda81fe53abe214
Sanskar00 <saxenasanskar090901@gmail.com>
2022-02-13 00:34:55 +0530

    Some more changes

--- client/src/components/ProfilePageComponent/PersonalInfoPage.js
+++ client/src/components/ProfilePageComponent/PersonalInfoPage.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useContext } from "react/cjs/react.development";
+import React, { useState, useContext } from "react";
+
 import { AuthContext } from "../../context/AuthContext";
 
 const PersonalInfoPage = () => {
